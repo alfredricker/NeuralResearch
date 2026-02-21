adc556918bb9b6c5b7de2350ab12cbceeef607ea
alfred ricker <alfred.ricker7@gmail.com>
2026-02-21 16:58:10 -0500

    logos example

---
+++ compiler/target/debug/deps/compiler-2757c5b3b79b9cf2.d
@@ -0,0 +1,5 @@
+/home/fred/Projects/neural/research/compiler/target/debug/deps/compiler-2757c5b3b79b9cf2.d: src/main.rs
+
+/home/fred/Projects/neural/research/compiler/target/debug/deps/libcompiler-2757c5b3b79b9cf2.rmeta: src/main.rs
+
+src/main.rs:
---
+++ compiler/target/debug/deps/compiler-d832cbeea8955cb6.d
@@ -0,0 +1,5 @@
+/home/fred/Projects/neural/research/compiler/target/debug/deps/compiler-d832cbeea8955cb6.d: src/main.rs
+
+/home/fred/Projects/neural/research/compiler/target/debug/deps/libcompiler-d832cbeea8955cb6.rmeta: src/main.rs
+
+src/main.rs:
