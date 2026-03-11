861ba36b51bdcdc1a498e1dd020568bf6358dbb1
Alfred Ricker <alfredricker@Alfreds-MacBook-Pro.local>
2026-03-10 19:49:41 -0700

    target stuff

--- compiler/target/debug/deps/compiler-8f47d0e4c0e588cd.d
+++ compiler/target/debug/deps/compiler-8f47d0e4c0e588cd.d
@@ -1,6 +1,6 @@
-/Users/alfredricker/Project/neural/NeuralResearch/compiler/target/debug/deps/compiler-8f47d0e4c0e588cd.d: src/lib.rs src/ast/mod.rs src/ast/graph.rs src/ast/io.rs src/ast/link.rs src/ast/display.rs src/ast/defaults.rs src/ast/statement.rs src/ast/block.rs src/ast/program.rs src/lexer/mod.rs src/lexer/token.rs src/parser/mod.rs src/parser/error.rs src/parser/link.rs src/parser/io.rs src/parser/arg.rs
+/Users/alfredricker/Project/neural/NeuralResearch/compiler/target/debug/deps/compiler-8f47d0e4c0e588cd.d: src/lib.rs src/ast/mod.rs src/ast/graph.rs src/ast/io.rs src/ast/link.rs src/ast/display.rs src/ast/defaults.rs src/ast/statement.rs src/ast/block.rs src/ast/program.rs src/lexer/mod.rs src/lexer/token.rs src/parser/mod.rs src/parser/error.rs src/parser/link.rs src/parser/io.rs src/parser/arg.rs src/parser/statement.rs src/parser/block.rs
 
-/Users/alfredricker/Project/neural/NeuralResearch/compiler/target/debug/deps/libcompiler-8f47d0e4c0e588cd.rmeta: src/lib.rs src/ast/mod.rs src/ast/graph.rs src/ast/io.rs src/ast/link.rs src/ast/display.rs src/ast/defaults.rs src/ast/statement.rs src/ast/block.rs src/ast/program.rs src/lexer/mod.rs src/lexer/token.rs src/parser/mod.rs src/parser/error.rs src/parser/link.rs src/parser/io.rs src/parser/arg.rs
+/Users/alfredricker/Project/neural/NeuralResearch/compiler/target/debug/deps/libcompiler-8f47d0e4c0e588cd.rmeta: src/lib.rs src/ast/mod.rs src/ast/graph.rs src/ast/io.rs src/ast/link.rs src/ast/display.rs src/ast/defaults.rs src/ast/statement.rs src/ast/block.rs src/ast/program.rs src/lexer/mod.rs src/lexer/token.rs src/parser/mod.rs src/parser/error.rs src/parser/link.rs src/parser/io.rs src/parser/arg.rs src/parser/statement.rs src/parser/block.rs
 
 src/lib.rs:
 src/ast/mod.rs:
@@ -19,3 +19,5 @@ src/parser/error.rs:
 src/parser/link.rs:
 src/parser/io.rs:
 src/parser/arg.rs:
+src/parser/statement.rs:
+src/parser/block.rs:
