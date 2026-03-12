6190da5eb371d53eddbc3eba4ab387e44e1885ce
alfred ricker <alfred.ricker7@gmail.com>
2026-03-12 17:34:04 -0400

    analyzer and neural

--- compiler/target/debug/deps/compiler-76f18620a6abf687.d
+++ compiler/target/debug/deps/compiler-76f18620a6abf687.d
@@ -1,6 +1,6 @@
-/home/fred/Projects/neural/research/compiler/target/debug/deps/compiler-76f18620a6abf687.d: src/lib.rs src/ast/mod.rs src/ast/graph.rs src/ast/io.rs src/ast/link.rs src/ast/display.rs src/ast/defaults.rs src/ast/statement.rs src/ast/block.rs src/ast/program.rs src/lexer/mod.rs src/lexer/token.rs src/parser/mod.rs src/parser/error.rs src/parser/link.rs src/parser/io.rs src/parser/arg.rs src/parser/statement.rs src/parser/block.rs
+/home/fred/Projects/neural/research/compiler/target/debug/deps/compiler-76f18620a6abf687.d: src/lib.rs src/ast/mod.rs src/ast/graph.rs src/ast/io.rs src/ast/link.rs src/ast/display.rs src/ast/defaults.rs src/ast/statement.rs src/ast/block.rs src/ast/program.rs src/ast/var.rs src/ast/expr.rs src/ast/op.rs src/lexer/mod.rs src/lexer/token.rs src/parser/mod.rs src/parser/error.rs src/parser/link.rs src/parser/var.rs src/parser/io.rs src/parser/arg.rs src/parser/statement.rs src/parser/block.rs src/parser/expr.rs
 
-/home/fred/Projects/neural/research/compiler/target/debug/deps/libcompiler-76f18620a6abf687.rmeta: src/lib.rs src/ast/mod.rs src/ast/graph.rs src/ast/io.rs src/ast/link.rs src/ast/display.rs src/ast/defaults.rs src/ast/statement.rs src/ast/block.rs src/ast/program.rs src/lexer/mod.rs src/lexer/token.rs src/parser/mod.rs src/parser/error.rs src/parser/link.rs src/parser/io.rs src/parser/arg.rs src/parser/statement.rs src/parser/block.rs
+/home/fred/Projects/neural/research/compiler/target/debug/deps/libcompiler-76f18620a6abf687.rmeta: src/lib.rs src/ast/mod.rs src/ast/graph.rs src/ast/io.rs src/ast/link.rs src/ast/display.rs src/ast/defaults.rs src/ast/statement.rs src/ast/block.rs src/ast/program.rs src/ast/var.rs src/ast/expr.rs src/ast/op.rs src/lexer/mod.rs src/lexer/token.rs src/parser/mod.rs src/parser/error.rs src/parser/link.rs src/parser/var.rs src/parser/io.rs src/parser/arg.rs src/parser/statement.rs src/parser/block.rs src/parser/expr.rs
 
 src/lib.rs:
 src/ast/mod.rs:
@@ -12,12 +12,17 @@ src/ast/defaults.rs:
 src/ast/statement.rs:
 src/ast/block.rs:
 src/ast/program.rs:
+src/ast/var.rs:
+src/ast/expr.rs:
+src/ast/op.rs:
 src/lexer/mod.rs:
 src/lexer/token.rs:
 src/parser/mod.rs:
 src/parser/error.rs:
 src/parser/link.rs:
+src/parser/var.rs:
 src/parser/io.rs:
 src/parser/arg.rs:
 src/parser/statement.rs:
 src/parser/block.rs:
+src/parser/expr.rs:
